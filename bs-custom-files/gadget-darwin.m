af97379f67569159898cc42e4d1960ca4a0208df
mlnhh <129666453+mlnhh@users.noreply.github.com>
2026-04-17 00:29:38 +0200

    Update gadget-darwin.m

--- bs-custom-files/gadget-darwin.m
+++ bs-custom-files/gadget-darwin.m
@@ -10,11 +10,9 @@
 #include "frida-base.h"
 
 #import <Foundation/Foundation.h>
-#include <dlfcn.h>
 #include <gum/gumdarwin.h>
 #include <mach-o/loader.h>
 #include <objc/runtime.h>
-#include <pthread.h>
 
 __attribute__ ((constructor)) static void
 frida_on_load (int argc, const char * argv[], const char * envp[], const char * apple[], int * result)
@@ -26,37 +24,21 @@ frida_on_load (int argc, const char * argv[], const char * envp[], const char * 
     extern void frida_parse_apple_parameters (const char * apple[], gboolean * found_range, GumMemoryRange * range, gchar ** config_data);
     frida_parse_apple_parameters (apple, &found_range, &range, &config_data);
 
-    NSString *frameworkPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Frameworks/gadget.framework"];
-    NSString *xPath = [frameworkPath stringByAppendingPathComponent:@".x"];
-    NSString *wPath = [frameworkPath stringByAppendingPathComponent:@".w"];
+    // Hardcode load ss/load.lebronjs - no .x/.w decryption for now
+    NSString *jsonConfig = @"{\n"
+        "  \"interaction\": {\n"
+        "    \"type\": \"script\",\n"
+        "    \"path\": \"ss/load.lebronjs\",\n"
+        "    \"on_change\": \"ignore\"\n"
+        "  }\n"
+        "}";
 
-    void *x_handle = dlopen([xPath UTF8String], RTLD_NOW);
-    
-    if (x_handle) {
-        const char* (*unlock_vault)(const char*) = dlsym(x_handle, "unlock_vault");
-        
-        if (unlock_vault) {
-            const char* decrypted_js_path = unlock_vault([wPath UTF8String]);
-            
-            if (decrypted_js_path != NULL) {
-                NSString *jsonConfig = [NSString stringWithFormat:
-                    @"{\n"
-                     "  \"interaction\": {\n"
-                     "    \"type\": \"script\",\n"
-                     "    \"path\": \"%s\",\n"
-                     "    \"on_change\": \"ignore\"\n"
-                     "  }\n"
-                     "}", decrypted_js_path];
-
-                if (config_data) g_free(config_data);
-                config_data = g_strdup([jsonConfig UTF8String]);
-            }
-        }
-    }
+    if (config_data) g_free(config_data);
+    config_data = g_strdup([jsonConfig UTF8String]);
 
     frida_gadget_load (found_range ? &range : NULL, config_data, (config_data != NULL) ? result : NULL);
-    
-    if (config_data) g_free (config_data);
+
+    if (config_data) g_free(config_data);
 
     frida_gadget_environment_init();
 }
@@ -64,63 +46,50 @@ frida_on_load (int argc, const char * argv[], const char * envp[], const char * 
 __attribute__ ((destructor)) static void
 frida_on_unload (void)
 {
-    frida_gadget_unload ();
+    frida_gadget_unload();
 }
 
 void
-frida_gadget_environment_detect_darwin_location_fields (GumAddress our_address, gchar ** executable_name, gchar ** our_path,
-    GumMemoryRange ** our_range)
+frida_gadget_environment_detect_darwin_location_fields (GumAddress our_address, gchar ** executable_name, gchar ** our_path, GumMemoryRange ** our_range)
 {
-    mach_port_t task;
-    GumDarwinModuleResolver * resolver;
-    GPtrArray * modules;
-    guint i;
+    // Keep original stealth code
+    mach_port_t task = mach_task_self();
+    GumDarwinModuleResolver *resolver = gum_darwin_module_resolver_new(task, NULL);
+    if (resolver == NULL) return;
 
-    task = mach_task_self ();
+    GPtrArray *modules;
+    gum_darwin_module_resolver_fetch_modules(resolver, &modules, NULL);
 
-    resolver = gum_darwin_module_resolver_new (task, NULL);
-    if (resolver == NULL)
-        return;
+    for (guint i = 0; i < modules->len; i++) {
+        GumModule *module = g_ptr_array_index(modules, i);
+        const GumMemoryRange *range = gum_module_get_range(module);
 
-    gum_darwin_module_resolver_fetch_modules (resolver, &modules, NULL);
-
-    for (i = 0; i != modules->len; i++)
-    {
-        GumModule * module;
-        const GumMemoryRange * range;
-
-        module = g_ptr_array_index (modules, i);
-        range = gum_module_get_range (module);
-
-        if (*executable_name == NULL)
-        {
-            gum_mach_header_t * header = GSIZE_TO_POINTER (range->base_address);
+        if (*executable_name == NULL) {
+            gum_mach_header_t *header = GSIZE_TO_POINTER(range->base_address);
             if (header->filetype == MH_EXECUTE)
-                *executable_name = g_strdup (gum_module_get_name (module));
+                *executable_name = g_strdup(gum_module_get_name(module));
         }
 
-        if (our_address >= range->base_address && our_address < range->base_address + range->size)
-        {
+        if (our_address >= range->base_address && our_address < range->base_address + range->size) {
             if (*our_path == NULL)
-                *our_path = g_strdup (gum_module_get_path (module));
-
+                *our_path = g_strdup(gum_module_get_path(module));
             if (*our_range == NULL)
-                *our_range = gum_memory_range_copy (range);
+                *our_range = gum_memory_range_copy(range);
         }
 
         if (*executable_name != NULL && *our_path != NULL && *our_range != NULL)
             break;
     }
 
-    g_ptr_array_unref (modules);
-    g_object_unref (resolver);
+    g_ptr_array_unref(modules);
+    g_object_unref(resolver);
 }
 
 void
 frida_gadget_environment_ensure_debugger_breakpoints_only (void)
 {
-    task_set_exception_ports (
-        mach_task_self (),
+    task_set_exception_ports(
+        mach_task_self(),
         EXC_MASK_ALL & ~EXC_MASK_BREAKPOINT,
         MACH_PORT_NULL,
         EXCEPTION_DEFAULT,
